07871dad2dfb98c8f5543bd08a43db574188e61b
Chen Frenkel <chenf156@gmail.com>
2016-11-18 22:09:00 +0200

    adjusted plugin for sublime text 3. Now, the first call to chef_run command sets the shell command to use, and the second call runs the set shell command (and so on). This was done because begin_edit() is no longer exposed...

--- runCommand.py
+++ runCommand.py
@@ -4,7 +4,6 @@ from os.path import dirname, realpath
 
 class ChefRunCommand(sublime_plugin.TextCommand):
 
-	curView = None
 	initialRegion = None
 	inputPanelView = None
 
@@ -16,27 +15,26 @@ class ChefRunCommand(sublime_plugin.TextCommand):
 
 	historyIndex = -1
 
-	selfChangeExpected = False
+	curCommand = None
 
-	def execute(self, command):
-		print "chef called with: " + command
-		#curView = sublime.active_window().active_view()
-		#sel = self.curView.sel()
+	def execute(self, command, edit):
+		print("chef called with: " + command)
 		region = self.initialRegion
 		if region.empty():
-			region = self.curView.line(region)
-		regionText = self.curView.substr(region)
-		print "got regionText: " + regionText
+			region = self.view.line(region)
+		regionText = self.view.substr(region)
+		print("got regionText: " + regionText)
 		p = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-		p.stdin.write(regionText)
+		p.stdin.write(regionText.encode('utf-8'))
 		p.stdin.close()
-		ret = p.stdout.read()
-		print ret
+		ret = p.stdout.read().decode('utf-8')
+		print(ret)
 		jmap = None
 		with open(self.pluginFilename, 'r') as historyFile:
 			jmap = json.load(historyFile)
 			jhistory = jmap.get(self.KEY_HISTORY)
-			if jhistory is not None and jhistory[len(jhistory)-1] != command:
+			print("jhistory: ", jhistory)
+			if jhistory is not None and (len(jhistory) < 1 or jhistory[len(jhistory)-1] != command):
 				jhistory.append(command)
 				if len(jhistory) > self.HISTORY_MAX_SIZE:
 					jhistory = jhistory[1:]
@@ -44,47 +42,45 @@ class ChefRunCommand(sublime_plugin.TextCommand):
 		if jmap is not None:
 			with open(self.pluginFilename, 'w') as historyFile:
 				json.dump(jmap, historyFile)
-		edit = self.curView.begin_edit()
-		self.curView.replace(edit, region, ret)
-		self.curView.end_edit(edit)
+		# edit = self.view.begin_edit()
+		self.view.replace(edit, region, ret)
+		# self.view.end_edit(edit)
 
 	def run(self, edit):
-		#self.view.insert(edit, 0, "Hello, World!")
-		print "hello chef"
-		self.curView = sublime.active_window().active_view()
-		sel = self.curView.sel()
-		print "sel: "; print sel[0]
+		print("hello chef")
+		sel = self.view.sel()
+		print("sel: ", sel[0])
 		self.initialRegion = sublime.Region(sel[0].a, sel[0].b)
 		self.historyIndex = -1 #meaning not showing history yet
 		if len(sel) > 1:
-			print "more then one selection, aborting"
+			print("more then one selection, aborting")
 			sublime.message_dialog("cannot run on more than ONE selection")
 		else:
-			self.inputPanelView = sublime.active_window().show_input_panel("chef| run command on input", "json_pp -f json", self.execute, self.onChange, None)
+			if self.curCommand != None:
+				self.execute(self.curCommand, edit)
+				self.curCommand = None
+			else:
+				self.inputPanelView = self.view.window().show_input_panel("chef| run command on input", "json_pp -f json", self.setCommand, self.onChange, None)
+
+	def setCommand(self, command):
+		self.curCommand = command
 
 	# beware of change recurssion!!!
 	def onChange(self, change):
-		print "change: " + change
-		if self.selfChangeExpected:
-			print 'self change. do nothing.'
-			self.selfChangeExpected = False
-		else:
-			if self.inputPanelView is not None and change.find('~~') != -1:
-				with open(self.pluginFilename, 'r') as historyFile:
-					jmap = json.load(historyFile)
-					jhistory = jmap.get(self.KEY_HISTORY)
-					if jhistory is not None:
-						if self.historyIndex == -1:
-							self.historyIndex = len(jhistory)-1
-							replaceCommand = jhistory[self.historyIndex]
-						elif self.historyIndex > 0:
-							self.historyIndex -= 1
-							replaceCommand = jhistory[self.historyIndex]
-						else: # == 0
-							self.historyIndex -= 1
-							replaceCommand = ''
-						self.selfChangeExpected = True
-						edit = self.inputPanelView.begin_edit()
-						self.inputPanelView.replace(edit, self.inputPanelView.visible_region(), replaceCommand)
-						self.inputPanelView.end_edit(edit)
-				
+		print("change: " + change)
+		if self.inputPanelView is not None and change.find('~~') != -1:
+			with open(self.pluginFilename, 'r') as historyFile:
+				jmap = json.load(historyFile)
+				jhistory = jmap.get(self.KEY_HISTORY)
+				if jhistory is not None:
+					if self.historyIndex == -1:
+						self.historyIndex = len(jhistory)-1
+						replaceCommand = jhistory[self.historyIndex]
+					elif self.historyIndex > 0:
+						self.historyIndex -= 1
+						replaceCommand = jhistory[self.historyIndex]
+					else: # == 0
+						self.historyIndex -= 1
+						replaceCommand = ''
+					self.inputPanelView = self.view.window().show_input_panel("chef| run command on input", replaceCommand, self.setCommand, self.onChange, None)
+			
